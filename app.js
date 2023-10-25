465c80c14d43e7b8f234a10f04ad57a1ba5c86e7
Ring Madat <smithemma329@gmail.com>
2023-10-25 05:28:13 +0300

    main

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const notesContainer = document.querySelector('.notes-container');
 const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorALL(".input-Box");
+
+createBtn.addEventListener("click", ()=> {
+    let inputBox = document.createElement("p");
+})
